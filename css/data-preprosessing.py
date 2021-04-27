078fbb5ec6a0d1a4ec50c78f201c38969b35edf5
Donghui Bak <dh.tonybak@gmail.com>
2021-04-27 15:30:32 +0900

    testing

--- css/data-preprosessing.py
+++ css/data-preprosessing.py
@@ -1,2 +1,29 @@
 import pandas as pd
 import numpy as np
+
+# raw data import
+raw_data = pd.read_csv("C:/Users/DH/Desktop/202101/Project/Data/data.txt", sep='\t', encoding='CP949')
+
+# 데이터 확인
+raw_data.head()
+raw_data.info()
+print(raw_data.columns.tolist())
+raw_data.shape
+
+# 코드 명세서 import
+ifrs_code = pd.read_csv("C:/Users/DH/Desktop/202101/Project/Data/K-IFRS_code.txt", sep='\t', encoding='CP949')
+
+# 데이터 확인
+ifrs_code.head()
+print(ifrs_code.columns.tolist())
+
+# 부도기업리스트 import
+budo_data = pd.read_csv("C:/Users/DH/Desktop/202101/Project/Data/부도기업리스트.txt", sep='\t', encoding='CP949')
+
+# 데이터 확인
+budo_data.head()
+budo_data.info()
+
+# 정상기업리스트 import
+normal_data = pd.read_csv("C:/Users/DH/Desktop/202101/Project/Data/정상기업리스트.txt", sep='\t', encoding='CP949')
+
